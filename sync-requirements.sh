13dc457650d749c70c6a3277195eb333160b009b
Justin Patrin <papercrane@reversefold.com>
2019-05-10 10:52:03 -0700

    Remove pip-tools scripts

--- sync-requirements.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ex
-pip-sync requirements.txt dev-requirements.txt build-requirements.txt
--- update-requirements.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -ex
-pip-compile --upgrade requirements.in -o requirements.txt
-pip-compile --upgrade build-requirements.in -o build-requirements.txt
-pip-compile --upgrade requirements.in dev-requirements.in build-requirements.in -o dev-requirements.txt
